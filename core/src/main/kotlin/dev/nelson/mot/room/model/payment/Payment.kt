28c776ac7e675c76894f7ca3fc4edbce745871e0
alexandr.sushkov <alexandr.sushkov.mail@gmail.com>
2018-07-30 20:05:13 +0300

    entity payment reworked.

--- core/src/main/kotlin/dev/nelson/mot/room/model/payment/Payment.kt
+++ core/src/main/kotlin/dev/nelson/mot/room/model/payment/Payment.kt
@@ -2,16 +2,19 @@ package dev.nelson.mot.room.model.payment
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.PrimaryKey
+import dev.nelson.mot.room.model.category.Category
 import dev.nelson.mot.room.model.category.CategoryTable
+import java.time.OffsetDateTime
 
-@Entity(tableName = PaymentTable.TABLE_NAME)
-class Payment(@ColumnInfo(name = PaymentTable.TITLE) var title: String,
-              @ColumnInfo(name = PaymentTable.SUMMARY) var summary: String,
-              @ColumnInfo(name = PaymentTable.CATEGORY_ID) val categoryId: Int,
-              //todo add date as column
-              @ColumnInfo(name = PaymentTable.COST) val cost: Long) {
+@Entity(tableName = PaymentTable.TABLE_NAME,
+        foreignKeys = [(ForeignKey(entity = Category::class, parentColumns = arrayOf(CategoryTable.CATEGORY_ID), childColumns = arrayOf(PaymentTable.CATEGORY_ID), onDelete = ForeignKey.SET_NULL))])
+data class Payment(@PrimaryKey(autoGenerate = true)
+                   @ColumnInfo(name = CategoryTable.CATEGORY_ID) val id: Long? = null,
+                   @ColumnInfo(name = PaymentTable.TITLE) var title: String,
+                   @ColumnInfo(name = PaymentTable.SUMMARY) var summary: String,
+                   @ColumnInfo(name = PaymentTable.CATEGORY_ID) val categoryId: Int,
+                   @ColumnInfo(name = PaymentTable.DATE) val date: OffsetDateTime,
+                   @ColumnInfo(name = PaymentTable.COST) val cost: Long)
 
-    @ColumnInfo(name = CategoryTable.CATEGORY_ID)
-    @PrimaryKey(autoGenerate = true) var id: Long = 0
-}
---
+++ core/src/main/kotlin/dev/nelson/mot/utils/typeconverter/MotTypeConverter.kt
@@ -0,0 +1,21 @@
+package dev.nelson.mot.utils.typeconverter
+
+import android.arch.persistence.room.TypeConverter
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
+object MotTypeConverter {
+    private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+
+    @TypeConverter
+    @JvmStatic
+    fun toOffsetDateTime(value: String?): OffsetDateTime? {
+        return value?.let { return formatter.parse(value, OffsetDateTime::from) }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun fromOffsetDateTime(date: OffsetDateTime?): String? {
+        return date?.format(formatter)
+    }
+}
