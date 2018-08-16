3ffb0922272f1cbe7cf11c28504a000b92f6b2e3
alexandr.sushkov <alexandr.sushkov.mail@gmail.com>
2018-08-16 14:45:28 +0300

    dependencies update.

--- core/src/main/kotlin/dev/nelson/mot/room/model/payment/Payment.kt
+++ core/src/main/kotlin/dev/nelson/mot/room/model/payment/Payment.kt
@@ -9,7 +9,10 @@ import dev.nelson.mot.room.model.category.CategoryTable
 import java.time.OffsetDateTime
 
 @Entity(tableName = PaymentTable.TABLE_NAME,
-        foreignKeys = [(ForeignKey(entity = Category::class, parentColumns = arrayOf(CategoryTable.CATEGORY_ID), childColumns = arrayOf(PaymentTable.CATEGORY_ID), onDelete = ForeignKey.SET_NULL))])
+        foreignKeys = [(ForeignKey(entity = Category::class,
+                parentColumns = arrayOf(CategoryTable.CATEGORY_ID),
+                childColumns = arrayOf(PaymentTable.CATEGORY_ID),
+                onDelete = ForeignKey.SET_NULL))])
 data class Payment(@PrimaryKey(autoGenerate = true)
                    @ColumnInfo(name = CategoryTable.CATEGORY_ID) val id: Long? = null,
                    @ColumnInfo(name = PaymentTable.TITLE) var title: String,
