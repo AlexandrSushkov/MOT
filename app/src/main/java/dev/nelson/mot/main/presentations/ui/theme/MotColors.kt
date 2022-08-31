6a9c52e32627876f8823f725740054540daf4ed9
Alexandr Sushkov <alexandr.sushkov.mail@gmail.com>
2022-08-31 15:22:55 +0300

    Simple line chart composable.

--- app/src/main/java/dev/nelson/mot/main/presentations/ui/theme/MotColors.kt
+++ app/src/main/java/dev/nelson/mot/main/presentations/ui/theme/MotColors.kt
@@ -16,5 +16,16 @@ object MotColors{
     val FavoriteButtonOnBackground = OrangeMaterial500
     val FavoriteButtonOffBackground = Color.LightGray
 
+    val chartColors = listOf(
+        Color(0xFFFF9E80),
+        Color(0xFFFFD180),
+        Color(0xFFFFFF8D),
+        Color(0xFFCCFF90),
+        Color(0xFFA7FFEB),
+        Color(0xFF80D8FF),
+        Color(0xFF8C9EFF),
+        Color(0xFFB388FF),
+        Color(0xFFFF80AB)
+    )
 
 }
