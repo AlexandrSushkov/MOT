ae4e230e8594070041b8104d67370f7436909e03
Alexandr Sushkov <alexandr.sushkov.mail@gmail.com>
2022-09-02 15:48:56 +0300

    Splash screen migrated to Android 12’s Splash Screen API. Simple icon with amination added.

--- app/src/main/java/dev/nelson/mot/main/presentations/splash/SplashActivity.kt
+++ app/src/main/java/dev/nelson/mot/main/presentations/splash/SplashActivity.kt
@@ -1,14 +1,20 @@
 package dev.nelson.mot.main.presentations.splash
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.nelson.mot.main.presentations.home.MainActivityCompose
+import kotlinx.coroutines.coroutineScope
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        startActivity(MainActivityCompose.getIntent(this))
-        finish()
+        splash.setOnExitAnimationListener{
+            startActivity(MainActivityCompose.getIntent(this))
+            finish()
+        }
     }
 }
