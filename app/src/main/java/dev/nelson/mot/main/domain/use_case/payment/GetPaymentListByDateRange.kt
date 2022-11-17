a7db5e3ad09f3be6f9832f1ba0ab17e549c5d6a0
Alexandr Sushkov <alexandr.sushkov.mail@gmail.com>
2022-11-17 20:12:08 +0200

    GetPaymentListByDateRange added.

---
+++ app/src/main/java/dev/nelson/mot/main/domain/use_case/payment/GetPaymentListByDateRange.kt
@@ -0,0 +1,36 @@
+package dev.nelson.mot.main.domain.use_case.payment
+
+import dev.nelson.mot.main.data.mapers.copyWith
+import dev.nelson.mot.main.data.mapers.toPaymentList
+import dev.nelson.mot.main.data.model.Payment
+import dev.nelson.mot.main.data.repository.PaymentRepository
+import dev.nelson.mot.main.domain.use_case.date_and_time.FormatTimeUseCase
+import dev.nelson.mot.main.util.Order
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+/**
+ * By default it returns list of [Payment] for current month sorted by the time, starting from the latest added [Payment].
+ */
+class GetPaymentListByDateRange @Inject constructor(
+    private val paymentRepository: PaymentRepository,
+    private val formatTimeUseCase: FormatTimeUseCase
+) {
+
+    fun execute(startTime: Long, endTime: Long? = null, order: Order = Order.Ascending): Flow<List<Payment>> {
+        val paymentsWithCategoryList = endTime?.let { paymentRepository.getPaymentsWithCategoryByDateRangeOrdered(startTime, it, order) }
+            ?: paymentRepository.getPaymentsWithCategoryByDateRangeOrdered(startTime, order)
+        return paymentsWithCategoryList
+            .map { it.toPaymentList() }
+            .map { it.formatDate() }
+    }
+
+    /**
+     * transform epoch mills into string date according to time system zone
+     */
+    private fun List<Payment>.formatDate(): List<Payment> {
+        return this.map { payment -> payment.copyWith(date = formatTimeUseCase.execute(payment.dateInMills)) }
+    }
+
+}
