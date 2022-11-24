1d8858f7f0c25e9bab807f973d4a881ff604dd93
Alexandr Sushkov <alexandr.sushkov.mail@gmail.com>
2022-11-24 16:47:25 +0200

    SingleLiveEvent removed.

--- app/src/main/java/dev/nelson/mot/main/presentations/home/HomeViewModel.kt
+++ app/src/main/java/dev/nelson/mot/main/presentations/home/HomeViewModel.kt
@@ -3,13 +3,11 @@ package dev.nelson.mot.main.presentations.home
 import androidx.databinding.ObservableField
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.nelson.mot.main.presentations.base.BaseViewModel
-import dev.nelson.mot.main.util.SingleLiveEvent
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : BaseViewModel() {
 
     val title = ObservableField<String>()
-    val onItemClickEvent: SingleLiveEvent<Unit> = SingleLiveEvent()
 
 }
